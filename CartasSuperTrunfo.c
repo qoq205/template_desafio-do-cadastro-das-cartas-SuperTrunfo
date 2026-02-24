e2249215ac53e86702ef02b63e52019194d93f79
qoq205 <davilopesdeabreu4@gmail.com>
2026-02-24 21:24:26 +0000

    Adicionei mais um printf para testar o codigo. Teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   printf("Olá mundo, no CodeSpace\n");
   printf("Exemplo de edição. Teste\n");
+  printf("Confirmar\n");
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
